82a888f70baddedfdc31770169b5e6060b2a57f4
tureraosantosh <tureraosantosh@gmail.com>
2022-02-05 17:24:56 +0530

    boot verion downgrade

--- supercure-1/src/main/java/com/supercure/configuration/SwaggerConfig.java
+++ supercure-1/src/main/java/com/supercure/configuration/SwaggerConfig.java
@@ -1,15 +1,7 @@
-/*package com.supercure.configuration;
-
-import java.util.ArrayList;
-import java.util.List;
+package com.supercure.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.hateoas.client.LinkDiscoverer;
-import org.springframework.hateoas.client.LinkDiscoverers;
-import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
-import org.springframework.plugin.core.SimplePluginRegistry;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -18,23 +10,11 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
- class SwaggerConfig {
-	
-	
-	@Primary
-    @Bean
-    public LinkDiscoverers discoverers() {
-        List<LinkDiscoverer> plugins = new ArrayList<>();
-        plugins.add(new CollectionJsonLinkDiscoverer());
-        return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
-    }
+class SwaggerConfig {
+
 	@Bean
 	public Docket appinfo() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.groupName("super").select()
-				.paths(PathSelectors.any())
-				.build();
+		return new Docket(DocumentationType.SWAGGER_2).groupName("super").select().paths(PathSelectors.any()).build();
 	}
 
 }
-*/
